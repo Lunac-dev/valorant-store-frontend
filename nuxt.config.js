c2ce5f075394fd19b53738dc0ed493ae6f7cf4a9
lunac-dev <lunac.info.c@gmail.com>
2022-07-15 22:52:45 +0900

    Fxied 3

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,8 +47,7 @@ export default {
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify',
-    '@nuxtjs/pwa',
-    '@aceforth/nuxt-optimized-images',
+    '@nuxtjs/pwa'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
