5babb0bedbd4e21d7f0dcb87ab5a937672806870
lunac-dev <lunac.info.c@gmail.com>
2023-04-19 19:03:54 +0900

    :+1: index

--- pages/index.vue
+++ pages/index.vue
@@ -1,83 +1,49 @@
 <template>
   <div>
-    <div class="mtop" v-bind:style="mtop2">
-      <div style="padding: 20px;">
-        <h2>
-          FEATURED
-        </h2>
-        <h1 class="mtop-h1">
-          {{ bundlename }}
-        </h1>
-        <h2>
-          COLLECTION // {{ price }} VP
-        </h2>
-      </div>
-    </div>
-    <v-container class="text-center">
-      <div>
-        <div style="text-align: center">
-          <adsbygoogle
-            :ad-slot="'6190084110'"
-            :ad-format="'horizontal'"
-          />
-        </div>
-        <h1 class="text-decoration-underline" style="font-family: 'VALORANT', sans-serif;">
-          {{ bundlename }}
-        </h1>
-        <v-row class="pt-5">
-          <v-col v-for="weapon in weapons" v-bind:key="weapon.name" cols="12" sm="3">
-            <v-card
-              class="mx-auto"
-              max-width="100%"
-              hover
-              @click="viewskin(weapon.uuid)"
+    <v-container fluid fill-height class="mtop">
+      <v-row>
+        <v-col class="text-center">
+          <h1 class="primary--text" style="font-family: 'VALORANT', sans-serif; font-size: 7vh">Valorant Store Checker</h1>
+          <h1>Making <span class="valofont">Valorant</span> Life More Comfortable</h1>
+          <v-btn
+            href="https://discord.com/api/oauth2/authorize?client_id=962376379658297375&permissions=83968&scope=bot%20applications.commands"
+            color="primary"
+            outlined
+            x-large
+          >
+            <v-icon
+              left
             >
-              <v-img
-                :src="weapon.displayIcon"
-                height="250px"
-                class="align-end"
-                aspect-ratio="1.4"
-                contain
-              >
-                <v-card-title>
-                  {{ weapon.name }}
-                </v-card-title>
-                <v-card-subtitle class="text-h6 text-decoration-underline text-right">
-                  {{ weapon.price }} VP
-                </v-card-subtitle>
-              </v-img>
-            </v-card>
-          </v-col>
-        </v-row>
-      </div>
-      <div class="pt-5">
-        <div style="text-align: center">
-          <adsbygoogle
-            :ad-slot="'4143888168'"
-            :ad-format="'horizontal'"
-          />
-        </div>
-        <h1 class="text-decoration-underline" style="font-family: 'VALORANT', sans-serif;">
-          Game News
-        </h1>
-        <v-row class="pt-5">
-          <v-col v-for="tmp in news" v-bind:key="tmp.name" cols="12" sm="3">
-            <v-card
-              class="mx-auto"
+              mdi-robot-excited-outline
+            </v-icon>
+            {{ $t('index_btn1') }}
+          </v-btn>
+          <v-btn
+            color="primary"
+            x-large
+            :to="localePath('store')"
+          >
+            <v-icon
+              left
             >
-              <a :href="tmp.url" target="_blank">
-                <v-img
-                  :src="tmp.banner_url"
-                  max-height="150px"
-                />
-              </a>
-              <v-card-subtitle>
-                {{ tmp.title }}
-              </v-card-subtitle>
-            </v-card>
-          </v-col>
-        </v-row>
-      </div>
+              mdi-cart
+            </v-icon>
+            {{ $t('index_btn2') }}
+          </v-btn>
+          <v-btn
+            color="primary"
+            x-large
+            href="https://apps.apple.com/us/app/store-checker-for-valorant/id1637273546"
+          >
+            <v-icon
+              left
+            >
+              mdi-apple
+            </v-icon>
+            {{ $t('index_btn3') }}
+          </v-btn>
+        </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
@@ -86,15 +52,9 @@
 export default {
   name: 'IndexPage',
 
-  async asyncData ({ $axios }) {
-    const response = await $axios.$get('/bundles')
-    const response2 = await $axios.$get('/news')
-    return { bundlename: response[0].name, price: response[0].price, weapons: response[0].weapons, mtop2: { backgroundImage: "url('" + response[0].displayIcon + "')" }, news: response2 }
-  },
-
-  methods: {
-    viewskin (uuid) {
-      window.open('/skin/' + uuid, '_self')
+  head () {
+    return {
+      title: 'Home'
     }
   }
 }
@@ -104,14 +64,15 @@ export default {
 .mtop {
   width: 100%;
   height: 100vh;
+  background: linear-gradient(to bottom, rgba(0, 0, 0, 0.3) 0%, rgba(0, 0, 0, 0.7) 75%, #000 100%), url("/img/bg2.jpg");
   background-position: center;
   background-repeat: no-repeat;
   background-attachment: scroll;
   background-size: cover;
 }
-.mtop-h1 {
+
+.valofont {
   color: #ff4655;
   font-family: 'VALORANT', sans-serif;
-  font-size: 8vh
 }
 </style>
