60af1effb352b710970f44e755b898b8a5b08288
lunac-dev <lunac.info.c@gmail.com>
2023-04-29 17:09:09 +0900

    see you

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout'
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline">
+          👋 Site is temporarily closed.
+        </v-card-title>
+        <v-card-text>
+          <p>
+            We have decided to temporarily close our site based on a DMCA notice from Riot Games.
+            Thank you for a year and a bit.
+          </p>
+          <p>
+            But we have not given up yet. We are confident that we are not doing any harm to Riot Games, but rather providing a convenience and even a benefit to our players.
+            Also, the DMCA notice is clearly wrong in many respects.
+            So we will talk to Riot Games and try to resolve it.
+          </p>
+          <p>
+            If you have any questions, please contact us through the official <a href="https://discord.gg/valorantstore-net"> Discord </a>.
+          </p>
+          <div class="text-xs-right">
+            <em><small>&mdash; Valorant Store Community</small></em>
+          </div>
+          <hr class="my-3" />
+          <a
+            href="https://discord.gg/valorantstore-net"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Discord
+          </a>
+          <br />
+          <a
+            href="https://apps.apple.com/us/app/store-checker-for-valorant/id1637273546"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            iOS App
+          </a>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
